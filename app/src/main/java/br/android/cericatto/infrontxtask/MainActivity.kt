ae6b02d4236974381f385cb45136513a91e1bacf
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2022-01-16 03:25:00 -0300

    Network connection check implemented.

--- app/src/main/java/br/android/cericatto/infrontxtask/MainActivity.kt
+++ app/src/main/java/br/android/cericatto/infrontxtask/MainActivity.kt
@@ -2,32 +2,44 @@ package br.android.cericatto.infrontxtask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import br.android.cericatto.infrontxtask.adapter.ViewPagerAdapter
 import br.android.cericatto.infrontxtask.databinding.ActivityMainBinding
+import br.android.cericatto.infrontxtask.util.isNetworkConnected
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val titlesArray = arrayOf("Fixtures", "Results")
 
-    val titlesArray = arrayOf(
-        "Fixtures",
-        "Results"
-    )
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setViewPager()
+        checkNetwork()
+    }
+
+    private fun checkNetwork() {
+        if (isNetworkConnected()) {
+            setViewPager()
+        } else {
+            Snackbar.make(binding.root, "Network down.\nClick here to try to connect again.", 10000)
+                .setAction("Here") {
+                    checkNetwork()
+                }.show()
+        }
     }
 
     private fun setViewPager() {
         val viewPager = binding.viewPager
         val tabLayout = binding.tabLayout
+        tabLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_500))
         tabLayout.setSelectedTabIndicatorHeight(10)
 
         val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
--- app/src/main/java/br/android/cericatto/infrontxtask/util/DateUtils.kt
+++ app/src/main/java/br/android/cericatto/infrontxtask/util/DateUtils.kt
@@ -7,31 +7,6 @@ import java.util.*
 
 const val SUFFIX = ":00"
 
-/**
- * Input: 2019-02-02T15:00:00.000Z
- * Output: Feb 2, 2019 at 15:00
- */
-fun String.formatDateToAdapter(): String {
-    val a = this.split("-")
-    // 2019
-    // 02
-    // 02T15:00:00.000Z
-    val b = a[2].split("T")
-    // 02
-    // 15:00:00.000Z
-    val c = b[1].split(".")
-    // 15:00:00
-    // 000Z
-    val d = c[0].split(":")
-    // 15
-    // 00
-    // 00
-    var monthDayString = b[0]
-    var monthDay = b[0].toInt()
-    if (monthDay < 10) monthDayString = b[0].toInt().toString()
-    return "${a[1].formatMonth()} $monthDayString, ${a[0]} at ${d[0]}:${d[1]}"
-}
-
 fun String.weekday(context: Context): String {
     val isoFormat = SimpleDateFormat(context.getString(R.string.iso_8601_format))
     val dateInIso = isoFormat.parse(this)
@@ -39,6 +14,10 @@ fun String.weekday(context: Context): String {
     return weekdayFormat.format(dateInIso).getStringWithoutLastChar()
 }
 
+/**
+ * Input: 2019-02-02T15:00:00.000Z
+ * Output: Feb 2, 2019 at 15:00
+ */
 fun String.formatDateToAdapter(context: Context): String {
     val isoFormat = SimpleDateFormat(context.getString(R.string.iso_8601_format))
     val dateInIso = isoFormat.parse(this)
@@ -61,24 +40,6 @@ fun String.dayOfMonth(): String {
     val a = this.split("-")
     val b = a[2].split("T")
     return b[0].dayTwoDigits()
-}
-
-fun String.formatMonth(): String {
-    return when(this) {
-        "01" -> "Jan"
-        "02" -> "Feb"
-        "03" -> "Mar"
-        "04" -> "Apr"
-        "05" -> "May"
-        "06" -> "Jun"
-        "07" -> "Jul"
-        "08" -> "Aug"
-        "09" -> "Sep"
-        "10" -> "Oct"
-        "11" -> "Nov"
-        "12" -> "Dec"
-        else -> ""
-    }
 }
 
 fun String.dayTwoDigits(): String {
